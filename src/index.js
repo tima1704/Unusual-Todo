0a96d353bc0f813ac01fd159b8c265c7198882d9
Timur Ibatullin <ibatullintima@gmail.com>
2023-05-01 01:54:41 +0600

    fix: React.StrickMode on Fragment from index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
 
